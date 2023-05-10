2ce48202639e87d5474be51346cb58c7fd0666f4
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-05-09 21:31:16 -0600

    Agrega el label y opciones de monedas

--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -1,7 +1,21 @@
-const useSelectMonedas = () => {
-  const SelectMonedas = () => {
-    console.log("Desde Select Monedas")
-  }
+import styled from "@emotion/styled"
+
+const Label = styled.label`
+  color: #fff;
+`
+
+const useSelectMonedas = (label, opciones) => {
+  const SelectMonedas = () => (
+    <>
+      <Label>{label}</Label>
+      <select>
+        <option value=''>Seleccione</option>
+        {opciones.map((opcion) => (
+          <option key={opcion.id} value={opcion.id}>{opcion.nombre}</option>
+        ))}
+      </select>
+    </>
+  )
 
   return [SelectMonedas]
 }
