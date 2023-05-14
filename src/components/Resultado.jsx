fc845effef377acea5cddb58a9a5a66348e37355
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-05-14 14:36:32 -0600

    Añade estilos a los resultados de cotización

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,26 +1,59 @@
 import styled from "@emotion/styled"
 
+const Contenedor = styled.div`
+  color: #fff;
+  font-family: "Lato", sans-serif;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`
+const Imagen = styled.img`
+  display:block;
+  width: 120px;
+`
+
+const Texto = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: 700;
+  }
+`
+
+const Precio = styled.p`
+  font-size: 24px;
+  span {
+    font-weight: 700;
+  }
+`
+
 const Resultado = ({ resultado }) => {
   const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
     resultado
   return (
-    <div>
-      <p>
-        El precio es de: <span>{PRICE}</span>
-      </p>
-      <p>
-        Precio mas alto del dia: <span>{HIGHDAY}</span>
-      </p>
-      <p>
-        Precio mas bajo del dia: <span>{LOWDAY}</span>
-      </p>
-      <p>
-        Variación ultimas 24 horas: <span>{CHANGEPCT24HOUR}</span>
-      </p>
-      <p>
-        Ultima actualización <span>{LASTUPDATE}</span>
-      </p>
-    </div>
+    <Contenedor>
+      <Imagen
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt='Imagen cripto'
+      ></Imagen>
+      <div>
+        <Precio>
+          El precio es de: <span>{PRICE}</span>
+        </Precio>
+        <Texto>
+          Precio mas alto del dia: <span>{HIGHDAY}</span>
+        </Texto>
+        <Texto>
+          Precio mas bajo del dia: <span>{LOWDAY}</span>
+        </Texto>
+        <Texto>
+          Variación ultimas 24 horas: <span>{CHANGEPCT24HOUR}</span>
+        </Texto>
+        <Texto>
+          Ultima actualización <span>{LASTUPDATE}</span>
+        </Texto>
+      </div>
+    </Contenedor>
   )
 }
 
